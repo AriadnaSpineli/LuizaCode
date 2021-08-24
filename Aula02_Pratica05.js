87e12a40610bd2fa5d46a09f8957c21afad61f7d
AriadnaSpineli <ariadna.spin@yahoo.com.br>
2021-08-24 20:09:14 -0300

    Exercio da aula 02 pratica 05

---
+++ Aula02_Pratica05.js
@@ -0,0 +1,7 @@
+var nome1 = 'Ariadna';
+var nome2 = 'Ariadna';
+
+var resultado = nome1 == nome2;
+var verificar = resultado == true ? 'igual' : 'diferente';
+
+console.log (`A string ${nome1} é ${verificar} a string ${nome2}.`);
