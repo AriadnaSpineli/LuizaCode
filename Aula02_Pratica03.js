fea0fd8e2278bda4b3dc72d08e9fe581e09fe2f4
AriadnaSpineli <ariadna.spin@yahoo.com.br>
2021-08-24 19:39:33 -0300

    Exercio da aula 02 pratica 03

---
+++ Aula02_Pratica03.js
@@ -0,0 +1,10 @@
+var n1 = 5;
+var n2 = 4;
+
+var media = (n1 + n2) / 2;
+var resultadoMedia = `Sua média é ${media}` 
+var resultado = (media >= 7) 
+    ? `${resultadoMedia}, parabéns você foi aprovada!` 
+    : `${resultadoMedia}, não foi aprovada, precisará se empenhar um pouco mais.`;
+
+console.log(resultado);
