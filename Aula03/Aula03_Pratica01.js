2ae3a038ded3822a21235f7c041144bb17fdfbc2
AriadnaSpineli <ariadna.spin@yahoo.com.br>
2021-08-24 22:00:41 -0300

    Exercio da aula 03 pratica 01

---
+++ Aula03/Aula03_Pratica01.js
@@ -0,0 +1,13 @@
+var nota1 = 4
+var nota2 = 5.5
+var media = (nota1 + nota2) / 2
+
+console.log (`A média é ${media}.`)
+
+if (media >= 7) {
+    console.log ('A situação final da aluna é APROVADA.')
+} else if (media < 5) {
+    console.log ("A situação final da aluna é REPROVADA. ")
+} else {
+    console.log ("A aluna irá fazer prova final.")
+}
