50900fff95cf266d6aead1af432a17be5128c9be
AriadnaSpineli <ariadna.spin@yahoo.com.br>
2021-08-25 22:15:13 -0300

    Exercio da aula 03 pratica 03

---
+++ Aula03/Aula03_Pratica03.js
@@ -0,0 +1,15 @@
+var letra = 'C'
+
+switch (letra) {
+    case 'B':
+        console.log ('Bola')
+        break
+    case 'C':
+        console.log ('Casa')
+        break
+    case 'A':
+        console.log ('Ariadna')
+        break
+    default:
+        console.log ('Não existe palavra com a letra selecionada')
+}
